6832ed4fc2cebb8149922bc7a6600b0d09f752f1
Giancarlo Cordero Ortiz <giancarlo.cordero.ortiz@sap.com>
2021-11-09 19:37:12 -0500

    Experimentation with cypress studio. Initial

--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/checkout/checkout-flow.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/checkout/checkout-flow.e2e-spec.ts
@@ -24,6 +24,60 @@ context('Checkout flow', () => {
       checkout.fillPaymentFormWithCheapProduct(user);
       checkout.placeOrderWithCheapProduct(user);
       checkout.verifyOrderConfirmationPageWithCheapProduct(user);
+
+      // Cypress Studio stuff: 
+      /* ==== Generated with Cypress Studio ==== */
+      //cy.get('.searchbox > input').clear();
+      //cy.get('.searchbox > input').type('video');
+      //cy.get(':nth-child(1) > .has-media > .name').click();
+      cy.visit('http://localhost:4200/electronics-spa/en/USD/product/266685/battery-video-light');
+      cy.get('[aria-label="Add one more"]').click();
+      cy.get('form.ng-untouched > .btn').click();
+      cy.get('.cx-dialog-buttons > .btn-primary').click();
+      cy.get('.cx-progress-button-container').click();
+      cy.get('.cx-card-link').click();
+      cy.get(':nth-child(2) > .cx-btn').click();
+      cy.get(':nth-child(3) > .cx-delivery-label > .cx-delivery-mode').click();
+      cy.get('#deliveryMode-premium-gross').check();
+      cy.get(':nth-child(2) > .btn').click();
+      cy.get(':nth-child(3) > .col-md-12 > .btn').click();
+      cy.get('.form-group.ng-untouched > :nth-child(1) > label > .ng-select > .ng-select-container').click();
+      cy.get('.form-group.ng-untouched > :nth-child(1) > label > .ng-select > .ng-select-container > .ng-value-container > .ng-input > input').click();
+      cy.get('#a26b90c06f49-3').click();
+      cy.get('.form-group.ng-touched > :nth-child(1) > label > .ng-select > .ng-select-container > .ng-value-container > .ng-input > input').click();
+      cy.get('.col-md-12 > :nth-child(2) > label > .form-control').clear();
+      cy.get('.col-md-12 > :nth-child(2) > label > .form-control').type('Jack Daniels');
+      cy.get(':nth-child(3) > label > .form-control').clear();
+      cy.get(':nth-child(3) > label > .form-control').type('4111111111111111');
+      cy.get(':nth-child(2) > .ng-select > .ng-select-container > .ng-arrow-wrapper').click();
+      cy.get(':nth-child(2) > .ng-select > .ng-select-container > .ng-value-container > .ng-input > input').click();
+      cy.get('#a26948c8c47d-1').click();
+      cy.get(':nth-child(2) > .ng-select > .ng-select-container > .ng-value-container > .ng-input > input').click();
+      cy.get(':nth-child(3) > .ng-select > .ng-select-container > .ng-value-container > .ng-input > input').click();
+      cy.get('#af09bf200622-2').click();
+      cy.get(':nth-child(3) > .ng-select > .ng-select-container > .ng-value-container > .ng-input > input').click();
+      cy.get('#cVVNumber').clear();
+      cy.get('#cVVNumber').type('234');
+      cy.get('.SideContent').click();
+      cy.get(':nth-child(5) > .form-check > label > .form-check-label').click();
+      cy.get(':nth-child(5) > .form-check > label > .form-check-input').check();
+      cy.get(':nth-child(2) > .btn').click();
+      cy.get('cx-mini-cart > a').click();
+      cy.get('.flyout > :nth-child(4) > cx-generic-link > a').click();
+      cy.get(':nth-child(2) > :nth-child(1) > :nth-child(2) > .row > .col-md-4 > cx-add-to-cart > .ng-untouched > .btn').click();
+      cy.get('.cx-dialog-buttons > .btn-primary').click();
+      cy.get('.cx-progress-button-container').click();
+      cy.get('.cx-card-link').click();
+      cy.get(':nth-child(2) > .cx-btn').click();
+      cy.get(':nth-child(3) > .cx-delivery-label > .cx-delivery-mode').click();
+      cy.get('#deliveryMode-premium-gross').check();
+      cy.get(':nth-child(2) > .btn').click();
+      cy.get(':nth-child(1) > .cx-payment-card-inner > cx-card > .cx-card > .card-body > .cx-card-actions > :nth-child(1) > div > .cx-card-link').click();
+      cy.get(':nth-child(2) > .btn').click();
+      cy.get('.scaled-input').check();
+      cy.get('.cx-place-order-form > .btn').click();
+      /* ==== End Cypress Studio ==== */
+
     });
   });
 });
--- projects/storefrontapp-e2e-cypress/cypress/integration/regression/homepage.e2e-spec.ts
+++ projects/storefrontapp-e2e-cypress/cypress/integration/regression/homepage.e2e-spec.ts
@@ -15,6 +15,17 @@ context('Homepage', () => {
     cy.get('cx-page-slot.Section1 cx-banner');
   });
 
+  it('should have consent preferences', () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('.btn-primary').click();
+    cy.get('.btn').should('contain.text', 'Consent');
+    cy.get('.btn').click();
+    
+    cy.get('.cx-dialog-header > h3').should('contain.text', 'Consent Management');
+    cy.get('.close > span > .cx-icon').click();
+    /* ==== End Cypress Studio ==== */
+  });
+  
   it('should have footer with footer navigation and notice', () => {
     cy.get('cx-page-slot.Footer').within(() => {
       cy.get('cx-navigation-ui > nav').should('have.length', 3);
@@ -25,5 +36,165 @@ context('Homepage', () => {
       'contain',
       'SAP SE or an SAP affiliate company. All rights reserved.'
     );
+  });
+
+  it('should validate main navigation', () => {
+    /* ==== Generated with Cypress Studio ==== */
+    // Brands
+    cy.get('.flyout > :nth-child(2) > [tabindex="0"]').click();
+    cy.get('nav.is-open > :nth-child(2) > cx-generic-link.all > .all').click();
+    cy.url().should('contain', 'brands');
+    cy.visit('/');
+
+    // Digital Cameras
+    cy.get('.flyout > :nth-child(3) > span').click();
+    //cy.get('nav.is-open > :nth-child(3) > cx-generic-link.all > .all').click();
+    cy.get('nav.is-open > .wrapper > cx-generic-link.all > .all').click();
+    cy.url().should('contain', 'Digital');
+    cy.visit('/');
+
+    // Film Cameras
+    cy.get('.flyout > :nth-child(4) > cx-generic-link > a').click();
+    cy.url().should('contain', 'Film');
+    cy.visit('/');
+        
+    // Camcorders
+    cy.get('.flyout > :nth-child(5) > cx-generic-link > a').click();
+    cy.url().should('contain', 'Camcorders');
+    cy.visit('/');
+
+    // Webcams
+    cy.get('.flyout > :nth-child(6) > cx-generic-link > a').click();
+    cy.url().should('contain', 'Webcams');
+    cy.visit('/');
+
+    // Accessories
+    cy.get(':nth-child(7) > [tabindex="0"]').click();
+    //cy.get('nav.is-open > :nth-child(7) > cx-generic-link.all > .all').click();
+    cy.get('nav.is-open > .wrapper > cx-generic-link.all > .all').click();
+    cy.url().should('contain', 'Accessories');
+    cy.visit('/');
+    /* ==== End Cypress Studio ==== */
+  
+  });
+
+  it('should validate carrousels', () => {
+    // Our Bestselling Products. Via arrows. Forward Navigation.
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get(':nth-child(1) > cx-carousel > .carousel-panel > .slides > .slide.active > :nth-child(1) > a > .is-initialized > img').click();
+    cy.visit('/');
+    cy.get(':nth-child(1) > cx-carousel > .carousel-panel > .next > .cx-icon').click();
+    cy.get(':nth-child(1) > cx-carousel > .carousel-panel > .slides > .slide.active > :nth-child(1) > a > .is-initialized > img').click();
+    cy.visit('/');
+    cy.get(':nth-child(1) > cx-carousel > .carousel-panel > .next > .cx-icon').click();
+    cy.get(':nth-child(1) > cx-carousel > .carousel-panel > .next > .cx-icon').click();
+    cy.get(':nth-child(1) > cx-carousel > .carousel-panel > .next > .cx-icon').click();
+    cy.get(':nth-child(1) > cx-carousel > .carousel-panel > .slides > .slide.active > :nth-child(1) > a > .is-initialized > img').click();
+    cy.visit('/');
+    /* ==== End Cypress Studio ==== */
+
+    // What's new. Via buttons. Forward Navigation
+    
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get(':nth-child(2) > cx-carousel > .carousel-panel > .slides > .slide.active > :nth-child(1) > a > .is-initialized > img').click();
+    //cy.get('a > .is-initialized > img').click();
+    cy.visit('/');
+    cy.get(':nth-child(2) > cx-carousel > .indicators > [aria-label="Slide 2"] > .cx-icon').click();
+    cy.get(':nth-child(2) > cx-carousel > .carousel-panel > .slides > .slide.active > :nth-child(1) > a > .is-initialized > img').click();
+    //cy.get('cx-generic-link > a > .is-initialized > img').click();
+    cy.visit('/');
+    cy.get(':nth-child(2) > cx-carousel > .indicators > [aria-label="Slide 3"] > .cx-icon').click();
+    cy.get(':nth-child(2) > cx-carousel > .carousel-panel > .slides > .slide.active > .item > a > .is-initialized > img').click();
+    //cy.get('a > .is-initialized > img').click();
+    cy.visit('/');
+    /* ==== End Cypress Studio ==== */
+
+  });
+
+  it('should validate category slots', () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('.Section2A > :nth-child(1) > cx-generic-link > a > .is-initialized > img').click();
+    cy.get('nav > :nth-child(2) > a').should('contain', 'Digital');
+    cy.visit('/');
+
+    cy.get('.Section2A > :nth-child(2) > cx-generic-link > a > .is-initialized > img').click();
+    cy.get('nav > :nth-child(2) > a').should('contain', 'Lenses');
+    cy.visit('/');
+
+    cy.get('.Section2B > :nth-child(1) > cx-generic-link > a > .is-initialized > img').click();
+    cy.get('nav > :nth-child(2) > a').should('contain', 'Camcorders');
+    cy.visit('/');
+
+    cy.get('.Section2B > :nth-child(2) > cx-generic-link > a > .is-initialized > img').click();
+    cy.get('nav > :nth-child(2) > a').should('contain', 'Supplies');
+    cy.visit('/');
+    /* ==== End Cypress Studio ==== */
+
+
+  });
+
+  it('should validate products visibility', () =>{
+    // Check first and last specific product in slot near footer. 
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('.Section4 > :nth-child(1) > cx-generic-link > a > .is-initialized > img').click();
+    cy.get('h1').should('contain', 'Web Camera');
+    cy.visit('/');
+
+    cy.get(':nth-child(4) > cx-generic-link > a > .is-initialized > img').click();
+    cy.get('h1').should('contain', 'Light');
+        cy.visit('/');
+    /* ==== End Cypress Studio ==== */
+  });
+
+  it('should validate all header components', () => {
+    /* ==== Generated with Cypress Studio ==== */
+    // Quick Order
+    cy.get('.SiteLinks > :nth-child(1) > cx-generic-link > a').click();
+    cy.url().should('contain', 'quick-order');
+    
+    cy.visit('/');
+
+    // Find a Store
+    cy.get('.SiteLinks > :nth-child(2) > cx-generic-link > a').click();
+    cy.url().should('contain', 'store-finder');
+    cy.get('.SiteLogo > cx-banner > cx-generic-link > a > .is-initialized > img').click();
+    
+    // Contact Us
+    cy.get('.SiteLinks > :nth-child(3) > cx-generic-link > a').click();
+    cy.url().should('contain', 'contact');
+    cy.visit('/');
+    
+    // Help
+    cy.get('.SiteLinks > :nth-child(4) > cx-generic-link > a').click();
+    cy.url().should('contain', 'faq');
+    cy.visit('/');
+    
+    // Change language
+    cy.get(':nth-child(1) > label > .small').click();
+    cy.get(':nth-child(1) > label > select').select('de');
+    cy.url().should('contain', 'de');
+    cy.get(':nth-child(1) > label > select').select('en');
+    cy.url().should('contain', 'en');
+
+    // Change currency
+    cy.get(':nth-child(2) > label > select').select('JPY');
+    cy.url().should('contain', 'JPY');
+    cy.get(':nth-child(2) > label > select').select('USD');
+    cy.url().should('contain', 'USD');
+
+    // Visit login page
+    cy.get('cx-login > a').click();
+    cy.url().should('contain', 'login');
+    cy.visit('/');
+    
+    // Visit empty cart
+    cy.get('.count').click();
+    cy.url().should('contain', 'cart');
+    cy.visit('/');
+
+    // Search box should be visible
+    cy.get('.searchbox').should('be.visible');
+    /* ==== End Cypress Studio ==== */
+
   });
 });
