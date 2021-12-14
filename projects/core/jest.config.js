047b630b1beb10f95d347b05246ea96c85ca3926
Patryk Smul <psmul@divante.pl>
2021-12-14 11:44:05 +0100

    basic jest config file for core

---
+++ projects/core/jest.config.js
@@ -0,0 +1,9 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('../../tsconfig.json')
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['../../jest.ts'],
+
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+}
